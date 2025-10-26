cbe9fe3210e23725ebf2b2bfb42ccc38372e74a9
Student-831 <0userhack110@gmail.com>
2025-10-26 21:14:12 +0300

    v0.1  ders1.2

--- 2_1.py
+++ 2_1.py
@@ -1,1 +1,8 @@
-print("naber")
+import random
+chars ="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+pass_length = int(input("Parolanin uzunlugunu belirtiniz: "))
+password = ""
+for x in range(pass_length):
+    password += random.choice(chars)
+
+print("Oluşturulan parola: ", password)
